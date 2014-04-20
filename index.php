5b9a47a76c608e5660bd5ecf05c714e1d0f462ca
Thomas Spycher <me@tspycher.com>
2014-04-20 15:54:20 +0200

    Adde Google Analytics

--- index.php
+++ index.php
@@ -193,6 +193,15 @@ include_once "code/Twitter.php";
 					</div>
 		
 			</div>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+            ga('create', '<?php echo $c->g_trackingId;?>', '<?php echo $c->g_id;?>');
+            ga('send', 'pageview');
+
+        </script>
 	</body>
 </html>
